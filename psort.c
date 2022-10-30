e4578618c07e2b9c061a0b709120734d2164f23d
Caroline Machart <machart@wisc.edu>
2022-10-30 11:40:37 -0500

    first commit

---
+++ psort.c
@@ -0,0 +1,14 @@
+
+/**
+ * Takes two arguments -> an input file and an output file
+ * The input file has records which are each 100 bytes: 
+ * We need to read in each 100 bytes into a data structure (maybe a dictionary) and store the first 4 bytes as 
+ * the key and the total 100 bytes as the value
+*/
+int main(int argc, char *argv[]) {
+
+}
+
+/**
+ * Sort method - should it take a dictionary as input and output a dictionary too? 
+*/
