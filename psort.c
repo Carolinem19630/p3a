8c443830cd4850656abb3373d44d794304e5b561
Caroline Machart <machart@wisc.edu>
2022-10-31 16:10:03 -0500

    implemented merge sort with the threads

--- psort.c
+++ psort.c
@@ -17,11 +17,79 @@ struct keyRecord * records;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; // lock for array of keyRecords
 
 int numThreads;
+int numRecords;
+
+void merge(int beg, int mid, int end)
+{
+    int i, j, k;
+    int n1 = mid - beg + 1;
+    int n2 = end - mid;
+ 
+    struct keyRecord *L = malloc(sizeof(struct keyRecord) * n1);
+    struct keyRecord *R = malloc(sizeof(struct keyRecord) * n2);
+ 
+    for (i = 0; i < n1; i++)
+        L[i] = records[beg + i];
+    for (j = 0; j < n2; j++)
+        R[j] = records[mid + 1 + j];
+ 
+    i = 0; 
+    j = 0; 
+    k = beg; 
+    pthread_mutex_lock(&lock);
+    while (i < n1 && j < n2) {
+        if (L[i].key <= R[j].key) {
+            records[k] = L[i];
+            i++;
+        }
+        else {
+            records[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        records[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    while (j < n2) {
+        records[k] = R[j];
+        j++;
+        k++;
+    }
+    pthread_mutex_unlock(&lock);
+}
+
+void sort(int beg, int end)
+{
+    if (beg < end) {
+        int mid = beg + (end - beg) / 2;
+ 
+        sort(beg, mid);
+        sort(mid + 1, end);
+ 
+        merge(beg, mid, end);
+    }
+}
+
 /**
  * Method where the threads start
 */
 void* mergeSort(void* args){
     int numThread = *(int*) args;
+
+    int beg = numRecords / numThreads * numThread;
+    int end = (numRecords/numThreads) * (numThread + 1) -1;
+    int mid = (end - beg) / 2 + beg;
+    if (beg < end){
+        sort(beg, mid);
+        sort(mid + 1, end);
+        merge(beg, mid, end); 
+    }
+
 }
 
 /**
@@ -37,7 +105,7 @@ int main(int argc, char *argv[]) {
     fseek(f, 0, SEEK_SET); 
 
     int fi = open(argv[1], O_RDONLY);
-    int numRecords = size / 100; 
+    numRecords = size / 100; 
     records = malloc(sizeof(struct keyRecord) * numRecords);
     char *file = (char *) mmap(NULL, size, PROT_READ, MAP_SHARED, fi,0); 
 
@@ -53,7 +121,6 @@ int main(int argc, char *argv[]) {
 
     numThreads = get_nprocs();
     pthread_t p[numThreads];
-    printf("num threads: %d\n", numThreads);
     int threadNum[numThreads];
     for (int i = 0; i < numThreads; i++){
         threadNum[i] = i; 
@@ -66,6 +133,7 @@ int main(int argc, char *argv[]) {
         pthread_join(p[j], NULL); 
     }
     
+    // todo: figure out how to merge all the threads' work
 
+    // todo: write records to file
 }
-
